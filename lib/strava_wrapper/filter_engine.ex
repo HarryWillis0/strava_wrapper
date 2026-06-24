702ebeca51262753e44200da2167f9f0c80c9fad
harry <>
2026-06-24 09:47:06 -0600

    feat: add aggregate stats computation to FilterEngine
    Closes #19

--- lib/strava_wrapper/filter_engine.ex
+++ lib/strava_wrapper/filter_engine.ex
@@ -16,4 +16,31 @@ defmodule StravaWrapper.FilterEngine do
 
   defp maybe_filter_type(activities, :not_set), do: activities
   defp maybe_filter_type(activities, type), do: Enum.filter(activities, &(&1.type == type))
+
+  @spec stats([%Activity{}]) :: map()
+  def stats(activities) do
+    Enum.reduce(
+      activities,
+      %{
+        total_count: 0,
+        total_distance: 0.0,
+        total_duration: 0,
+        first_date: nil,
+        type_breakdown: %{}
+      },
+      fn activity, acc ->
+        %{
+          total_count: acc.total_count + 1,
+          total_distance: acc.total_distance + (activity.distance || 0),
+          total_duration: acc.total_duration + (activity.duration || 0),
+          first_date: earliest_date(acc.first_date, activity.date),
+          type_breakdown: Map.update(acc.type_breakdown, activity.type, 1, &(&1 + 1))
+        }
+      end
+    )
+  end
+
+  defp earliest_date(nil, date), do: date
+  defp earliest_date(acc, nil), do: acc
+  defp earliest_date(acc, date), do: if(DateTime.compare(date, acc) == :lt, do: date, else: acc)
 end
